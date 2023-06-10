123c57d810ed50748e159846451aab011e27c242
Pierre Lasante <pierre.lasante@videotron.ca>
2023-06-10 11:42:11 -0700

    Fixs HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/NavbarAndFooter/HomePage/Carousel';
-import { Heros } from './layouts/NavbarAndFooter/HomePage/Heros';
-import { LibraryServices } from './layouts/NavbarAndFooter/HomePage/LibraryServices';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './HomePage/HomePage';
 
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <HomePage />
       <Footer />
     </div>
   );
 }
-
-export default App;
