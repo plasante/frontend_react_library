9191c4d60d1133e8324d57f9471b0643443074f0
Pierre Lasante <pierre.lasante@videotron.ca>
2023-07-06 09:25:12 -0400

    Okta is working

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-    clientId: '0oaa8x8m6g5mClTTz5d7',
-    issuer: 'https://dev-08866031.okta.com/oath2/default',
+    clientId: '0oaa9jl82dRSfAf315d7',
+    issuer: 'https://dev-16564339.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
