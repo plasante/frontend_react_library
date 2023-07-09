aab7a741740aef793a6014d4a3361ce66be6af55
Pierre Lasante <pierre.lasante@videotron.ca>
2023-07-09 11:16:09 -0400

    Before implementing LeaveAReview component

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -243,7 +243,7 @@ export const BookCheckoutPage = () => {
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount} 
                                           isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} 
-                                          checkoutBook={checkoutBook}/>
+                                          checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} />
                 </div>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -267,7 +267,7 @@ export const BookCheckoutPage = () => {
                 </div>
                 <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount} 
                                       isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} 
-                                      checkoutBook={checkoutBook}/>
+                                      checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} />
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
             </div>
--- src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -3,7 +3,7 @@ import BookModel from "../../models/BookModel";
 
 export const CheckoutAndReviewBox: React.FC<{
     book: BookModel | undefined, mobile: boolean, currentLoansCount: number,
-    isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any
+    isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any, isReviewLeft: boolean
 }> = (props) => {
 
     function buttonRender() {
@@ -23,6 +23,15 @@ export const CheckoutAndReviewBox: React.FC<{
             }
         }
         return (<Link to={'/login'} className="btn btn-success btn-lg">Sign in</Link>)
+    }
+
+    function reviewRender() {
+        if (props.isAuthenticated && !props.isReviewLeft) {
+            return (<p>Leave a review component here.</p>)
+        } else if (props.isAuthenticated && props.isReviewLeft) {
+            return (<p><b>Thank you for your review!</b></p>)
+        }
+        return (<div><hr /><p>Sign in to be able to leave a review.</p></div>)
     }
 
     return (
@@ -59,9 +68,7 @@ export const CheckoutAndReviewBox: React.FC<{
                 <p className="mt-3">
                     This number can change until placing order has been complete.
                 </p>
-                <p>
-                    Sign in to be able to leave a review.
-                </p>
+                {reviewRender()}
             </div>
         </div>
     );
