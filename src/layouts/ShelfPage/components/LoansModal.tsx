2ab40093cda7957d9fc8fefd79fd716523984560
Pierre Lasante <pierre.lasante@videotron.ca>
2023-07-22 09:43:52 -0400

    Implements Book Renew

--- src/layouts/ShelfPage/components/LoansModal.tsx
+++ src/layouts/ShelfPage/components/LoansModal.tsx
@@ -1,7 +1,7 @@
 import { ReturnBook } from "../../../HomePage/Components/ReturnBook";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 
-export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile: boolean, returnBook: any }> = (props) => {
+export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile: boolean, returnBook: any, renewLoan: any }> = (props) => {
     return (
         <div className='modal fade' id={props.mobile ? `mobilemodal${props.shelfCurrentLoan.book.id}` :
             `modal${props.shelfCurrentLoan.book.id}`} data-bs-backdrop='static' data-bs-keyboard='false'
@@ -55,7 +55,14 @@ export const LoansModal: React.FC<{ shelfCurrentLoan: ShelfCurrentLoans, mobile:
                                             aria-current='true'>
                                         Return Book
                                     </button>
-                                    <button data-bs-dismiss='modal'
+                                    <button 
+                                        onClick={
+                                            props.shelfCurrentLoan.daysLeft < 0 ?
+                                            (event) => event.preventDefault()
+                                            :
+                                            () => props.renewLoan(props.shelfCurrentLoan.book.id)
+                                        }
+                                        data-bs-dismiss='modal'
                                         className={
                                             props.shelfCurrentLoan.daysLeft < 0 ?
                                                 'list-group-item list-group-item-action inactiveLink' :
