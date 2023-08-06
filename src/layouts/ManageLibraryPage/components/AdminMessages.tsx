9b59868703b8e3dacaeb6996203f0e929c4c108f
Pierre Lasante <pierre.lasante@videotron.ca>
2023-08-06 08:39:11 -0400

    Implements admin able to answer user messages.

--- src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import MessageModel from "../../../models/MessageModel";
 import { SpinnerLoading } from "../../../utils/SpinnerLoading";
 import { Pagination } from "../../../utils/Pagination";
+import AdminMessageRequest from "../../../models/AdminMessageRequest";
+import { AdminMessage } from "./AdminMessage";
 
 export const AdminMessages = () => {
     
@@ -19,6 +21,9 @@ export const AdminMessages = () => {
     // Pagination
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0);
+
+    // Recall useEffect
+    const [btnSubmit, setBtnSubmit] = useState(false);
 
     useEffect(() => {
         const fetchUserMessages = async () => {
@@ -48,7 +53,7 @@ export const AdminMessages = () => {
         })
         window.scrollTo(0, 0);
         // eslint-disable-next-line
-    }, [authState, currentPage]);
+    }, [authState, currentPage, btnSubmit]);
 
     if (isLoadingMessages) {
         return(
@@ -64,6 +69,27 @@ export const AdminMessages = () => {
         );
     }
 
+    async function submitResponseToQuestion(id: number, response: string) {
+        const url = `http://localhost:8080/api/messages/secure/admin/message`;
+        if (authState && authState?.isAuthenticated && id !== null && response !== '') {
+            const messageAdminRequestModel: AdminMessageRequest = new AdminMessageRequest(id, response);
+            const requestOptions = {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(messageAdminRequestModel)
+            };
+
+            const messageAdminRequestModelResponse = await fetch(url, requestOptions);
+            if (!messageAdminRequestModelResponse.ok) {
+                throw new Error('Something went wrong');
+            }
+            setBtnSubmit(!btnSubmit);
+        }
+    }
+
     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
     
     return(
@@ -72,7 +98,7 @@ export const AdminMessages = () => {
                 <>
                     <h5>Pending Q/A: </h5>
                     {messages.map(message => (
-                        <p key={message.id}>Questions that need a response</p>
+                        <AdminMessage message={message} key={message.id} submitResponseToQuestion={submitResponseToQuestion}/>
                     ))}
                 </>
                 :
