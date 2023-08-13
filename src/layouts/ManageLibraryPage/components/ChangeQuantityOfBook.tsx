f1014ab96c645f28f3ac8ca150ccf74b2f921229
Pierre Lasante <pierre.lasante@videotron.ca>
2023-08-13 10:25:54 -0400

    Implements ChangeQuantityOfBook

--- src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
+import { useOktaAuth } from "@okta/okta-react";
 
 export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) => {
     
+    const { authState } = useOktaAuth();
     const [quantity, setQuantity] = useState<number>(0);
     const [remaining, setRemaining] = useState<number>(0);
 
@@ -13,6 +15,24 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) 
         }
         fetchBookInState();
     }, []);
+
+    async function increaseQuantity() {
+        const url = `http://localhost:8080/api/admin/secure/increase/book/quantity/?bookId=${props.book?.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if (!quantityUpdateResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+        setQuantity(quantity + 1);
+        setRemaining(remaining + 1);
+    }
     
     return (
         <div className='card mt-3 shadow p-3 mb-3 bg-body rounded'>
@@ -55,7 +75,7 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) 
                         <button className='m-1 btn btn-md btn-danger' >Delete</button>
                     </div>
                 </div>
-                <button className='m1 btn btn-md main-color text-white' >Add Quantity</button>
+                <button className='m1 btn btn-md main-color text-white' onClick={increaseQuantity}>Add Quantity</button>
                 <button className='m1 btn btn-md btn-warning' >Decrease Quantity</button>
             </div>
         </div>
