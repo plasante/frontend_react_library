873de08429890f6b2c94e0cd41027c34555eb7f4
Pierre Lasante <pierre.lasante@videotron.ca>
2023-08-15 09:05:57 -0400

    Implements ChangeQuantityOfBook

--- src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
+++ src/layouts/ManageLibraryPage/components/ChangeQuantityOfBook.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
 import { useOktaAuth } from "@okta/okta-react";
 
-export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) => {
+export const ChangeQuantityOfBook: React.FC<{ book: BookModel, deleteBook: any }> = (props, key) => {
     
     const { authState } = useOktaAuth();
     const [quantity, setQuantity] = useState<number>(0);
@@ -32,6 +32,41 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) 
         }
         setQuantity(quantity + 1);
         setRemaining(remaining + 1);
+    }
+
+    async function decreaseQuantity() {
+        const url = `http://localhost:8080/api/admin/secure/decrease/book/quantity/?bookId=${props.book?.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const quantityUpdateResponse = await fetch(url, requestOptions);
+        if (!quantityUpdateResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+        setQuantity(quantity - 1);
+        setRemaining(remaining - 1);
+    }
+
+    async function deleteBook() {
+        const url = `http://localhost:8080/api/admin/secure/delete/book/?bookId=${props.book?.id}`;
+        const requestOptions = {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const updateResponse = await fetch(url, requestOptions);
+        if (!updateResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+        props.deleteBook(); 
     }
     
     return (
@@ -72,11 +107,11 @@ export const ChangeQuantityOfBook: React.FC<{ book: BookModel }> = (props, key) 
                 </div>
                 <div className='mt-3 col-md-1'>
                     <div className='d-flex justify-content-start'>
-                        <button className='m-1 btn btn-md btn-danger' >Delete</button>
+                        <button className='m-1 btn btn-md btn-danger' onClick={deleteBook}>Delete</button>
                     </div>
                 </div>
                 <button className='m1 btn btn-md main-color text-white' onClick={increaseQuantity}>Add Quantity</button>
-                <button className='m1 btn btn-md btn-warning' >Decrease Quantity</button>
+                <button className='m1 btn btn-md btn-warning' onClick={decreaseQuantity}>Decrease Quantity</button>
             </div>
         </div>
     );
--- src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
+++ src/layouts/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
@@ -14,6 +14,8 @@ export const ChangeQuantityOfBooks = () => {
     const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
 
+    const [bookDelete, setBookDelete] = useState(false);
+
     useEffect(() => {
         const fetchBooks = async () => {
             const baseUrl: string = `http://localhost:8080/api/books?page=${currentPage - 1}&size=${booksPerPage}`;
@@ -54,7 +56,7 @@ export const ChangeQuantityOfBooks = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    }, [currentPage]);
+    }, [currentPage, bookDelete]);
 
     const indexOfLastBook: number = currentPage * booksPerPage;
     const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
@@ -62,6 +64,8 @@ export const ChangeQuantityOfBooks = () => {
         booksPerPage * currentPage : totalAmountOfBooks;
 
     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    const deleteBook = () => setBookDelete(!bookDelete);
 
     if (isLoading) {
         return (
@@ -88,7 +92,7 @@ export const ChangeQuantityOfBooks = () => {
                         {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
                     </p>
                     {books.map(book => (
-                        <ChangeQuantityOfBook book={book} key={book.id} />
+                        <ChangeQuantityOfBook book={book} key={book.id} deleteBook={deleteBook}/>
                     ))}
                 </>
                 :
