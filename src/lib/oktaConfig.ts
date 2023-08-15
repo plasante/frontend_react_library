5f5d7713d681ddb29a28364bca5db7fc9096255f
Pierre Lasante <pierre.lasante@videotron.ca>
2023-08-15 11:02:28 -0400

    Secured front-end

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oaa9jl82dRSfAf315d7',
     issuer: 'https://dev-16564339.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
